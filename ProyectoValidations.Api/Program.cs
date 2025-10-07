99560593ce7b0c73c3716d1fbe8f83af43a2e27f
John Rutherford <rutherford.jorge@gmail.com>
2025-10-07 18:13:09 -0300

    genero error

--- ProyectoValidations.Api/Program.cs
+++ ProyectoValidations.Api/Program.cs
@@ -21,6 +21,7 @@ namespace ProyectoValidations.Api
         {
             var builder = WebApplication.CreateBuilder(args);
 
+
             builder.Services.AddControllers();
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
